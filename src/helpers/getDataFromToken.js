144bf803c749ae0a0a195d37c4918f86f26e4bc8
Sagar~ <sagargope2001@gmail.com>
2024-04-16 02:56:03 +0530

    fix(/api/profile): bug fix

--- src/helpers/getDataFromToken.js
+++ src/helpers/getDataFromToken.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const getDataFromToken = (request) => {
   try {
-    const token = request?.cookies.get("token")?.value || "";
+    const token = request.cookies.get("token")?.value || "";
     const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
 
     return decodedToken.id;
